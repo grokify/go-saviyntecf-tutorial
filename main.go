47f632bd8d103036e07c8731e3f37a4c75f8b8a2
grokify <johncwang@gmail.com>
2024-05-09 07:17:32 -0700

    enhance: `main.go`: add `http.Server` timeouts

--- main.go
+++ main.go
@@ -3,14 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/grokify/go-saviyntecf-tutorial/server"
 )
 
 func main() {
+	timeout := 1 * time.Second
 	srv := &http.Server{
-		Addr:    ":8080",
-		Handler: server.NewHandler(),
+		Addr:              ":8080",
+		Handler:           server.NewHandler(),
+		IdleTimeout:       timeout,
+		ReadHeaderTimeout: timeout,
+		ReadTimeout:       timeout,
+		WriteTimeout:      timeout,
 	}
 	err := srv.ListenAndServe()
 	if err != nil {
